82785b0bb1b194131fedb50280f0faebc6d0125b
OhEsPaco <fcomanuel.garcia4@alu.uclm.es>
2017-10-03 10:58:25 +0200

    segundo commit de prueba

--- src/b1_03/B1_03.java
+++ src/b1_03/B1_03.java
@@ -17,7 +17,7 @@ public class B1_03 {
     public static void main(String[] args) {
         // TODO code application logic here
         //commit de prueba
-        System.out.println();
+        System.out.println("");
     }
     
 }
